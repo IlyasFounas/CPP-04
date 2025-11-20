8a3525332cb15d02dbaa629f1e637b62418ca78f
Ilyas Founas <ifounas@student.42lyon.fr>
2025-11-20 13:25:27 +0100

    ex02 started

---
+++ ex02/obj/Cat.d
@@ -0,0 +1,7 @@
+obj/Cat.o: srcs/class/Cat.cpp inc/Cat.hpp inc/Animal.hpp inc/Brain.hpp
+
+inc/Cat.hpp:
+
+inc/Animal.hpp:
+
+inc/Brain.hpp:
---
+++ ex02/obj/WrongCat.d
@@ -0,0 +1,8 @@
+obj/WrongCat.o: srcs/class/WrongCat.cpp inc/WrongCat.hpp \
+  inc/WrongAnimal.hpp inc/Animal.hpp
+
+inc/WrongCat.hpp:
+
+inc/WrongAnimal.hpp:
+
+inc/Animal.hpp:
